37803783acf899608108950608fa928f852c1840
Jordy Morales <dev.morales.jordy@gmail.com>
2024-09-26 10:08:45 -0400

    adding mongoose models, routes and controllers

--- src/routes/links.js
+++ src/routes/links.js
@@ -1,26 +1,11 @@
 const express = require('express')
 
+const { getLinks, createLink } = require('../controllers/links')
+
 const router = express.Router()
 
-const links = [
-    {
-        id: 1,
-        name: "Google",
-        url: "http://google.com",
-        visible: true,
-        archived: false
-    },
-    {
-        id: 2,
-        name: "Facebook",
-        url: "http://facebook.com",
-        visible: true,
-        archived: false
-    },
-]
+router.get('/links', getLinks)
 
-router.get('/links', (req, res) => {
-    res.json(links)
-})
+router.post('/links', createLink)
 
 module.exports = router
