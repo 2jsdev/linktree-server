7c78fadb9f70c17c53270b23601f3633e08673b2
Jordy Morales <dev.morales.jordy@gmail.com>
2024-10-02 10:56:52 -0400

    adding cors

--- src/server.js
+++ src/server.js
@@ -1,26 +1,28 @@
-const express = require('express')
-const dbConnection = require("./database/config")
+const express = require("express");
+const cors = require("cors");
+const dbConnection = require("./database/config");
 
 class Server {
-    constructor() {
-        this.app = express()
-        this.port = process.env.PORT || 4000;
-    }
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT || 4000;
+  }
 
-    middlewares() {
-        this.app.use(express.json())
-        this.app.use(require("./routes/links"))
-    }
-    
-    execute() {
-        dbConnection()
-        
-        this.middlewares()        
+  middlewares() {
+    this.app.use(express.json());
+    this.app.use(cors());
+    this.app.use(require("./routes/links"));
+  }
 
-        this.app.listen(this.port, () => {
-            console.log(`http://localhost:${this.port}`)
-        })
-    }
+  execute() {
+    dbConnection();
+
+    this.middlewares();
+
+    this.app.listen(this.port, () => {
+      console.log(`http://localhost:${this.port}`);
+    });
+  }
 }
 
-module.exports = Server
+module.exports = Server;
